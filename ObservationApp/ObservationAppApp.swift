51f8bc600726278dc4b3c66b2e8b0a4145b0c508
Jaime Escobar Martinez <jescobar@insulet.com>
2023-08-19 22:23:57 -0700

    Initial Commit

---
+++ ObservationApp/ObservationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationAppApp.swift
+//  ObservationApp
+//
+//  Created by Jaime Escobar on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ObservationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
